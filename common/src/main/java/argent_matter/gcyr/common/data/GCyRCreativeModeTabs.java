912590def58ff0622d860b27225868dca1710084
screret <68943070+screret@users.noreply.github.com>
2023-11-08 20:12:03 +0200

    update GT, disable space station packager JEI preview

--- common/src/main/java/argent_matter/gcyr/common/data/GCyRCreativeModeTabs.java
+++ common/src/main/java/argent_matter/gcyr/common/data/GCyRCreativeModeTabs.java
@@ -16,54 +16,12 @@ import static argent_matter.gcyr.api.registries.GCyRRegistries.REGISTRATE;
 
 public class GCyRCreativeModeTabs {
     public static RegistryEntry<CreativeModeTab> GCYR = REGISTRATE.defaultCreativeTab(GCyR.MOD_ID,
-                    builder -> builder.displayItems(new RegistrateDisplayItemsGenerator(GCyR.MOD_ID))
+                    builder -> builder.displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator(GCyR.MOD_ID, REGISTRATE))
                             .icon(GCyRItems.DYSON_CONSTRUCTION_DRONE::asStack)
                             .build())
             .register();
 
     public static void init() {
 
-    }
-
-    public static class RegistrateDisplayItemsGenerator implements CreativeModeTab.DisplayItemsGenerator {
-
-        public final String name;
-
-        public RegistrateDisplayItemsGenerator(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public void accept(@Nonnull CreativeModeTab.ItemDisplayParameters itemDisplayParameters, @Nonnull CreativeModeTab.Output output) {
-            var tab = REGISTRATE.get(name, Registries.CREATIVE_MODE_TAB);
-            for (var entry : REGISTRATE.getAll(Registries.BLOCK)) {
-                if (!REGISTRATE.isInCreativeTab(entry, tab))
-                    continue;
-                Item item = entry.get().asItem();
-                if (item == Items.AIR)
-                    continue;
-                if (item instanceof ComponentItem componentItem) {
-                    NonNullList<ItemStack> list = NonNullList.create();
-                    componentItem.fillItemCategory(tab.get(), list);
-                    list.forEach(output::accept);
-                } else {
-                    output.accept(item);
-                }
-            }
-            for (var entry : REGISTRATE.getAll(Registries.ITEM)) {
-                if (!REGISTRATE.isInCreativeTab(entry, tab))
-                    continue;
-                Item item = entry.get();
-                if (item instanceof BlockItem)
-                    continue;
-                if (item instanceof ComponentItem componentItem) {
-                    NonNullList<ItemStack> list = NonNullList.create();
-                    componentItem.fillItemCategory(tab.get(), list);
-                    list.forEach(output::accept);
-                } else {
-                    output.accept(item);
-                }
-            }
-        }
     }
 }
--- common/src/main/java/argent_matter/gcyr/common/data/GCyRMachines.java
+++ common/src/main/java/argent_matter/gcyr/common/data/GCyRMachines.java
@@ -123,7 +123,7 @@ public class GCyRMachines {
                     .where(' ', any())
                     .build()
             )
-            .shapeInfos(definition -> {
+            /*.shapeInfos(definition -> {
                 ArrayList<MultiblockShapeInfo> shapeInfo = new ArrayList<>();
                 MultiblockShapeInfo.ShapeInfoBuilder builder = MultiblockShapeInfo.builder()
                         .aisle("       ", "   S   ", "       ", "       ", "       ", "       ")
@@ -150,7 +150,7 @@ public class GCyRMachines {
                         .where('G', CASING_TEMPERED_GLASS)
                         .where('C', Blocks.WHITE_CONCRETE).build());
                 return shapeInfo;
-            })
+            })*/
             .workableCasingRenderer(GTCEu.id("block/casings/voltage/luv/side"),
                     GTCEu.id("block/multiblock/assembly_line"), false)
             .register();
