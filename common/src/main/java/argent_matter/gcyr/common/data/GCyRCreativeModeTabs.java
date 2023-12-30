4182281f520c4fc855c64130cb3a7831f5edb6c6
screret <68943070+screret@users.noreply.github.com>
2023-12-30 22:56:29 +0200

    fix: creative mode tab name

--- common/src/main/java/argent_matter/gcyr/common/data/GCyRCreativeModeTabs.java
+++ common/src/main/java/argent_matter/gcyr/common/data/GCyRCreativeModeTabs.java
@@ -1,15 +1,10 @@
 package argent_matter.gcyr.common.data;
 
 import argent_matter.gcyr.GCyR;
-import com.gregtechceu.gtceu.api.item.ComponentItem;
-import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.common.data.GTCreativeModeTabs;
 import com.tterrag.registrate.util.entry.RegistryEntry;
-import net.minecraft.core.NonNullList;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.*;
-
-import javax.annotation.Nonnull;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 
 import static argent_matter.gcyr.api.registries.GCyRRegistries.REGISTRATE;
 
@@ -18,6 +13,7 @@ public class GCyRCreativeModeTabs {
     public static RegistryEntry<CreativeModeTab> GCYR = REGISTRATE.defaultCreativeTab(GCyR.MOD_ID,
                     builder -> builder.displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator(GCyR.MOD_ID, REGISTRATE))
                             .icon(GCyRItems.DYSON_CONSTRUCTION_DRONE::asStack)
+                            .title(Component.literal("Gregicality Rocketry"))
                             .build())
             .register();
 
