2c6a85f19002cc9006b712ab16da06f219193912
screret <68943070+screret@users.noreply.github.com>
2024-04-01 14:06:34 +0300

    add space suit to armor tags

--- src/main/java/argent_matter/gcyr/data/tags/ItemTagLoader.java
+++ src/main/java/argent_matter/gcyr/data/tags/ItemTagLoader.java
@@ -1,16 +1,26 @@
 package argent_matter.gcyr.data.tags;
 
 import argent_matter.gcyr.common.data.GCyRItems;
+import com.tterrag.registrate.providers.RegistrateItemTagsProvider;
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagEntry;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.Tags;
 
 public class ItemTagLoader {
 
-    public static void init(RegistrateTagsProvider<Item> provider) {
+    public static void init(RegistrateItemTagsProvider provider) {
         provider.addTag(ItemTags.TRIM_TEMPLATES)
-                        .add(TagEntry.element(GCyRItems.SPACE_UPGRADE_SMITHING_TEMPLATE.getId()));
+                .add(GCyRItems.SPACE_UPGRADE_SMITHING_TEMPLATE.get());
+        provider.addTag(Tags.Items.ARMORS_HELMETS)
+                .add(GCyRItems.SPACE_SUIT_HELMET.get());
+        provider.addTag(Tags.Items.ARMORS_CHESTPLATES)
+                .add(GCyRItems.SPACE_SUIT_CHEST.get());
+        provider.addTag(Tags.Items.ARMORS_LEGGINGS)
+                .add(GCyRItems.SPACE_SUIT_LEGS.get());
+        provider.addTag(Tags.Items.ARMORS_BOOTS)
+                .add(GCyRItems.SPACE_SUIT_BOOTS.get());
     }
 
 }
