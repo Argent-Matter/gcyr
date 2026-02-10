0bec585cccf4ae306ec5dbffded33f1ca4d732ea
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-02-10 13:55:18 +0000

    Keep chip in UI

--- src/main/java/argent_matter/gcyr/common/machine/multiblock/RocketScannerMachine.java
+++ src/main/java/argent_matter/gcyr/common/machine/multiblock/RocketScannerMachine.java
@@ -63,9 +63,6 @@ public class RocketScannerMachine extends PlatformMultiblockMachine implements I
     @Override
     public ModularUI createUI(Player entityPlayer) {
         ModularUI modularUI = super.createUI(entityPlayer);
-        modularUI.registerCloseListener(() -> {
-            Block.popResource(getLevel(), getPos(), posSaveSlot.getStackInSlot(0).copyAndClear());
-        });
         modularUI.widget(new SlotWidget(posSaveSlot, 0, 149, 105));
         modularUI.widget(new ButtonWidget(129, 105, 18, 18, this::onSaveButtonClick)
                 .setButtonTexture(GuiTextures.BUTTON)
